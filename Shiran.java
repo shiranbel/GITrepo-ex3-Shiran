5cae3e4b96e56dbf7737826bda806ec631b38fea
Shiran Belachdev <shiranbel56@gmail.com>
2026-04-14 18:27:10 +0300

    first commit

---
+++ Shiran.java
@@ -0,0 +1,7 @@
+public class Shiran {
+    public static void main(String[] args) {
+        System.out.println("Shiran");
+        System.out.println("Shiran");
+        System.out.println("Shiran");
+    }
+}
